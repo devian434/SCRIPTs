7fce42d31a35ad5e23101b2cfbb62536dcb9cb26
devian434 <158588494+devian434@users.noreply.github.com>
2024-11-13 19:48:29 +0700

    Create FORINSTACNE2.asm

---
+++ FORINSTACNE2.asm
@@ -0,0 +1,20 @@
+section .data
+    event db "RemoteEvent"
+
+section .text
+    global _start
+
+_start:
+    call WaitForPlayerAdded
+
+WaitForPlayerAdded:
+    call ConnectPlayerAdded
+    ret
+
+ConnectPlayerAdded:
+    call FireAllClients
+    ret
+
+FireAllClients:
+    ret
+
