1a820771dd0f76b23f03bbefb18ff88ad7967763
devian434 <158588494+devian434@users.noreply.github.com>
2024-11-13 19:15:42 +0700

    Create GUI.lua

---
+++ GUI.lua
@@ -0,0 +1,7 @@
+text = script.Parent.Parent.TextLabel --แม่ของแม่จอง script มีลูกคือ TextLabel
+button = script.Parent --Button คือแม่ของ script นี้
+
+button.MouseButton1Click:Connect(function()
+	text.Text = "หมาทามโคตรกระจอก" --เปลี่ยนแม่ของแม่ของ TextLabel
+end)
+
